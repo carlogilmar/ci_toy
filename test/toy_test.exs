91a0a77109425a9b03c03bf463953bebaf9660ee
Carlo Gilmar <carlogilmar12@gmail.com>
2019-07-08 13:58:09 -0500

    Adding bad test

--- test/toy_test.exs
+++ test/toy_test.exs
@@ -2,7 +2,15 @@ defmodule ToyTest do
   use ExUnit.Case
   doctest Toy
 
-  test "greets the world" do
+  test "greets the world 1" do
     assert 2 == 2
+  end
+
+  test "greets the world 2" do
+    assert 2 == 2
+  end
+
+  test "greets the world 3" do
+    assert 2 == 3
   end
 end
